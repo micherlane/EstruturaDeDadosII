8379691f5bfd00f971ed79344ebada54da6964df
Tester User <testercod3@gmail.com>
2023-02-27 09:47:01 -0300

    Atividade 02

---
+++ Atividade-02/arvore.h
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+typedef int TipoItem;
+
+struct arvore {
+    TipoItem item;
+    arvore* esquerda;
+    arvore* direita;
+};
+
+class Arvore{
+    private:
+        
+    public:
+        arvore* raiz;
+        Arvore();
+        arvore* adicionarNo(arvore* esquerda, TipoItem item, arvore* direita);
+        void imprimirArvore(arvore* noRaiz);
+        int getAltura(arvore* noRaiz);
+        void imprimirEmNivel(arvore* noRaiz);
+        int totalDeFolhas(arvore* noRaiz);
+        bool temItem(arvore* noRaiz, TipoItem item);
+};
